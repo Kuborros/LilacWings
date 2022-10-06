2d2ff75b1a5ca8802f64bbbd2d84d2249f9bb4d9
Kuborros <kubo@catbat.rocks>
2022-10-07 00:56:49 +0200

    Add project files.

---
+++ LilacWings/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace LilacWings
+{
+    [BepInPlugin("com.kuborro.plugins.fp2.lilacwings", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            var harmony = new Harmony("com.kuborro.plugins.fp2.lilacwings");
+            harmony.PatchAll(typeof(Patch));
+        }
+    }
+
+
+    class Patch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(FPPlayer), nameof(FPPlayer.State_Init), MethodType.Normal)]
+        static void Postfix(ref bool ___hasSpecialItem)
+        {
+            if (GameObject.Find("Player 1").GetComponent<FPPlayer>().characterID.ToString() == "LILAC")
+            {
+                ___hasSpecialItem = true;
+            }
+        }
+
+    }
+}
