9b5c2b4b5f3a5f1c34927091b6d0a4729d289214
Kuborros <kubo@catbat.rocks>
2022-10-14 19:23:55 +0200

    Added a percent setting to disable Lilac sreaming at you.

--- LilacWings/Plugin.cs
+++ LilacWings/Plugin.cs
@@ -1,16 +1,22 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
+using System;
 using UnityEngine;
+using Random = System.Random;
 
 namespace LilacWings
 {
     [BepInPlugin("com.kuborro.plugins.fp2.lilacwings", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        public static ConfigEntry<int> configYellPercent;
         private void Awake()
         {
+            configYellPercent = Config.Bind("General", "YellChance", 10, new ConfigDescription("Set the percent of how often you want Lilac to scream when super boosting. 0 = Never, 100 = Every time", new AcceptableValueRange<int>(0, 100)));
             var harmony = new Harmony("com.kuborro.plugins.fp2.lilacwings");
             harmony.PatchAll(typeof(Patch));
+            harmony.PatchAll(typeof(Patch2));
         }
     }
 
@@ -25,6 +31,29 @@ namespace LilacWings
             {
                 ___hasSpecialItem = true;
             }
+        }
+
+    }
+
+    class Patch2
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(FPPlayer), nameof(FPPlayer.Action_PlaySoundUninterruptable), MethodType.Normal)]
+        static bool Prefix( ref AudioClip sfxClip, bool ___hasSpecialItem, AudioClip ___sfxBigBoostLaunch)
+        {
+            if (___hasSpecialItem && sfxClip == ___sfxBigBoostLaunch)
+            {
+                Random rnd = new Random();
+                int yell = rnd.Next(0, 100);
+                if (yell > Plugin.configYellPercent.Value)
+                {
+                    return false;
+                    //Simply not run the method at all
+                }
+            }
+            return true;
+
+
         }
 
     }
